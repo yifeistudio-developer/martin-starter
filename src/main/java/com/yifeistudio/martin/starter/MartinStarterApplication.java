f7dcfe84d2f41d35f464c18143436d5b6caed24f
弘毅 <develop@yifeistudio.com>
2022-04-22 17:22:49 +0800

    first commit

---
+++ src/main/java/com/yifeistudio/martin/starter/MartinStarterApplication.java
@@ -0,0 +1,13 @@
+package com.yifeistudio.martin.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartinStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MartinStarterApplication.class, args);
+    }
+
+}
