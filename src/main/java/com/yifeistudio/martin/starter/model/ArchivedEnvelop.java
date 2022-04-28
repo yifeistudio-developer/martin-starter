7c24b85afb8ef2ae1d6df11c8f2b078498c5794c
弘毅 <develop@yifeistudio.com>
2022-04-28 10:04:24 +0800

    增加归档模型

---
+++ src/main/java/com/yifeistudio/martin/starter/model/ArchivedEnvelop.java
@@ -0,0 +1,40 @@
+package com.yifeistudio.martin.starter.model;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * 归档信件
+ *
+ * @author : hongyi
+ * created at 2022/4/28 - 09:57
+ **/
+@Data
+public class ArchivedEnvelop {
+
+    /**
+     * ID
+     */
+    private Long id;
+
+    /**
+     * 归档标签
+     */
+    private Integer tags;
+
+    /**
+     * 归档大小
+     */
+    private Integer size;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createAt;
+
+    /**
+     * 归档数据
+     */
+    private byte[] data;
+}
