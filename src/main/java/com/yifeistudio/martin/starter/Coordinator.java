1c6aee29b6cf8445cf85cf0c0cbe9b16525b37e3
弘毅 <develop@yifeistudio.com>
2022-05-01 14:11:32 +0800

    RocketMqMessageChannel

--- src/main/java/com/yifeistudio/martin/starter/Coordinator.java
+++ src/main/java/com/yifeistudio/martin/starter/Coordinator.java
@@ -1,5 +1,7 @@
 package com.yifeistudio.martin.starter;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * 协调器
  *
@@ -7,5 +9,30 @@ package com.yifeistudio.martin.starter;
  * created at 2022/4/26 - 15:19
  **/
 public interface Coordinator {
+
+    /**
+     * 自增序号
+     *
+     * @return 自增序号
+     */
+    long incrAndGetSeq();
+
+    /**
+     * 获取排他锁
+     *
+     * @param key 锁键
+     * @return 是否锁定成功
+     */
+    boolean tryLock(String key);
+
+    /**
+     * 获取排他锁
+     *
+     * @param key 锁键
+     * @param timeout 超时时间
+     * @param timeUnit 超时单位
+     * @return 是否锁定成功
+     */
+    boolean tryLock(String key, long timeout, TimeUnit timeUnit);
 
 }
--- src/main/java/com/yifeistudio/martin/starter/vendor/DefaultCoordinator.java
+++ src/main/java/com/yifeistudio/martin/starter/vendor/DefaultCoordinator.java
@@ -2,12 +2,52 @@ package com.yifeistudio.martin.starter.vendor;
 
 import com.yifeistudio.martin.starter.Coordinator;
 
+import java.util.concurrent.TimeUnit;
+
 /**
- * File Description
+ * 默认协调器
  *
  * @author : hongyi
  * created at 2022/4/27 - 16:19
  **/
 public class DefaultCoordinator implements Coordinator {
 
+
+    /**
+     * 自增序号
+     *
+     * @return 自增序号
+     */
+    @Override
+    public long incrAndGetSeq() {
+
+        return 0;
+    }
+
+    /**
+     * 获取排他锁
+     *
+     * @param key 锁键
+     * @return 是否锁定成功
+     */
+    @Override
+    public boolean tryLock(String key) {
+
+        return false;
+    }
+
+    /**
+     * 获取排他锁
+     *
+     * @param key      锁键
+     * @param timeout  超时时间
+     * @param timeUnit 超时单位
+     * @return 是否锁定成功
+     */
+    @Override
+    public boolean tryLock(String key, long timeout, TimeUnit timeUnit) {
+
+
+        return false;
+    }
 }
