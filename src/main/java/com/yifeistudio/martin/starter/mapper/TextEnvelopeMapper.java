68a4b400d681bdc470655a4477ece5d8a66b016d
hongyi <develop@yifeistudio.com>
2023-09-11 14:51:28 +0800

    mybatisplus

---
+++ src/main/java/com/yifeistudio/martin/starter/mapper/TextEnvelopeMapper.java
@@ -0,0 +1,8 @@
+package com.yifeistudio.martin.starter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yifeistudio.martin.starter.model.TextEnvelope;
+
+public interface TextEnvelopeMapper extends BaseMapper<TextEnvelope> {
+
+}
